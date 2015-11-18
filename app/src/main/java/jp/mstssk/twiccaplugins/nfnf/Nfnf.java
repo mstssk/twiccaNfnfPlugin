fc053bceb291c9ce46f03577dd527bfd02e62420
mstssk <mstssk+github@gmail.com>
2015-11-18 09:35:29 +0900

    read only文字列の場合の挙動を改善。Toastに出す

--- app/src/main/java/jp/mstssk/twiccaplugins/nfnf/Nfnf.java
+++ app/src/main/java/jp/mstssk/twiccaplugins/nfnf/Nfnf.java
@@ -11,23 +11,23 @@ public class Nfnf extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         final int result;
         if (intent.getAction().equals(Intent.ACTION_PROCESS_TEXT)) {
+            final String text = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT);
+            final String newText = NfnfUtils.buildText(this, null, text, null);
             if (intent.getBooleanExtra(Intent.EXTRA_PROCESS_TEXT_READONLY, false)) {
-                Toast.makeText(this, "Read only!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, newText, Toast.LENGTH_LONG).show();
                 result = RESULT_CANCELED;
             } else {
-                String text = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT);
-                String newText = NfnfUtils.buildText(this, null, text, null);
                 intent.putExtra(Intent.EXTRA_PROCESS_TEXT, newText);
                 result = RESULT_OK;
             }
         } else { // twicca ACTION_EDIT_TWEET
-            String prefix = intent.getStringExtra("prefix");
-            String userInput = intent.getStringExtra("user_input");
-            String suffix = intent.getStringExtra("suffix");
-            String newText = NfnfUtils.buildText(this, prefix, userInput, suffix);
+            final String prefix = intent.getStringExtra("prefix");
+            final String userInput = intent.getStringExtra("user_input");
+            final String suffix = intent.getStringExtra("suffix");
+            final String newText = NfnfUtils.buildText(this, prefix, userInput, suffix);
             intent.putExtra(Intent.EXTRA_TEXT, newText);
             intent.putExtra("cursor", NfnfUtils.getLength(newText) - NfnfUtils.getLength(suffix));
             result = RESULT_OK;
